b0556c436ab74442b9702f214f539b66c5097b22
Yiğit Sadıç <yigitsadic@gmail.com>
2021-05-02 19:43:02 +0300

    no need for models

--- internal/model/game.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Game struct {
-	ID              string `json:"id"`
-	CurrentPrize    int    `json:"currentPrize"`
-	NextWinnerCheck string `json:"nextWinnerCheck"`
-	LastWinnerCheck string `json:"lastWinnerCheck"`
-	CreatedAt       string `json:"createdAt"`
-}
